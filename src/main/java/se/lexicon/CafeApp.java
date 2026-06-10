4f13471edcafc6b6c3f59544c88817b94c6b6f12
Lina Abu Hijleh <lina_abuhijleh@hotmail.com>
2026-06-10 09:29:46 +0200

    project setup + greet user method

--- src/main/java/se/lexicon/CafeApp.java
+++ src/main/java/se/lexicon/CafeApp.java
@@ -1,4 +1,13 @@
 package se.lexicon;
 
+import java.util.Scanner;
+
 public class CafeApp {
+
+    public static void greetUser(){
+        IO.println("Welcome! What is your name? ");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        IO.println("Hi " + name + "!");
+    }
 }
